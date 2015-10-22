0184903625d25941b8f73108b7e285cfaab41b5f
somename <someemail>
2015-10-22 11:58:33 +0300

    besouro automatic message

--- src/org/unioulu/tol/sqat2015/planetExplorer/tests/TestPlanetExplorer.java
+++ src/org/unioulu/tol/sqat2015/planetExplorer/tests/TestPlanetExplorer.java
@@ -8,6 +8,21 @@ public class TestPlanetExplorer {
 
 	@Test
 	public void testCreatePlanetExplorer() {
+		// act
+		PlanetExplorer explorer = new PlanetExplorer(100,100,"(5,5)(7,8)");
+		
+		// assert
+		assertEquals(100,explorer.xlocation());
+	}
+	
+	
+	@Test
+	public void testCommand() {
+		// act
+		createPlanetExplorer();
+				
+		// assert
+		assertEquals(6,explorer.);
 		PlanetExplorer explorer = new PlanetExplorer(100,100,"(5,5)(7,8)");
 	}
 }
