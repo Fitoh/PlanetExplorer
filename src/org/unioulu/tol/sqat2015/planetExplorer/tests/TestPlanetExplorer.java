81124ca843d18e12b7f0a8edc115884b0d11c2d0
somename <someemail>
2015-10-22 11:41:01 +0300

    besouro automatic message

--- src/org/unioulu/tol/sqat2015/planetExplorer/tests/TestPlanetExplorer.java
+++ src/org/unioulu/tol/sqat2015/planetExplorer/tests/TestPlanetExplorer.java
@@ -1,13 +1,13 @@
 package org.unioulu.tol.sqat2015.planetExplorer.tests;
 
 import static org.junit.Assert.*;
-
+import org.unioulu.tol.sqat2015.planetExplorer.*;
 import org.junit.Test;
 
 public class TestPlanetExplorer {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testCreatePlanetExplorer() {
+		PlanetExplorer explorer = new PlanetExplorer(100,100,"(5,5)(7,8)");
 	}
 }
